db15ea33a1b5687da01dba6d88239e64632d931e
闫亚荣 <evayan@atbgrouppl.com>
2021-03-30 15:17:08 +0800

    提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}测试数据</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: '这里是taro!'
+    }
+  }
+}
+</script>
